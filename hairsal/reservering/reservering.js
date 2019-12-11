32b358e16cce45aa1fa346969246f7ea84ef347e
Alexander Kievit <alexander.kievit@student.hu.nl>
2019-12-11 17:05:27 +0100

    Front-end bijna af

---
+++ hairsal/reservering/reservering.js
@@ -0,0 +1,13 @@
+let behandelingKeuze = document.getElementById('behandeling_optie');
+let kapperKeuze = document.getElementById('kapper_optie');
+let normaleKapper = document.getElementsByClassName('normale_kapper');
+let kleurenKapper = document.getElementById('kleuren_kapper');
+let sigrid = document.getElementById('sigrid');
+let danielle = document.getElementById('daniëlle');
+let sabine = document.getElementById('sabine');
+let jason = document.getElementById('jason');
+
+
+//kapperKeuze.hidden = true;
+
+
